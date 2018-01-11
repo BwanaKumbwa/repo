a932020e3dfd0037d37bd0ac5b2389e9d3b097ab
BwanaKumbwa <marcin.furtak2@gmail.com>
2018-01-11 13:02:26 +0100

    Add files via upload

--- source.cpp
+++ source.cpp
@@ -1,48 +1,64 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <ctime>
 
-int main ()
+int main (int argc, char** argv)
 {
+srand( time( NULL ) );
+pcl::PointCloud<pcl::PointXYZ> middle, output;
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  //pcl::PCLPointCloud2::Ptr cloud (new pcl::PCLPointCloud2 ());
+  //pcl::PCLPointCloud2::Ptr cloud_filtered (new pcl::PCLPointCloud2 ());
+//pcl::PointXYZ pomocniczy;
+  // Fill in the cloud data
+  //pcl::PCDReader reader;
 
-pcl::PointCloud<pcl::PointXYZ> cloud_a, cloud_b, output;
-  
-pcl::PointCloud<pcl::PointXYZ>::Ptr input (new pcl::PointCloud<pcl::PointXYZ>);
-if (pcl::io::loadPCDFile<pcl::PointXYZ> ("chair_3a.pcd", *input) == -1) //* load the file
-  {
-    PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
-    return (-1);
-  }
+std::string cloud_name;
+std::cout << "Pass the name of the cloud you'd like to cut to 2048 points" << std::endl;
+std::cin >> cloud_name;
+  // Replace the path below with the path where you saved your file
+  //reader.read (cloud_name + ".pcd", *cloud); // Remember to download the file first!
+if (pcl::io::loadPCDFile<pcl::PointXYZ> (cloud_name + ".pcd", *cloud) == -1){//* load the file
+PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
+return (-1);
+}
 
-std::cout << "Loaded "
-            << input->width * input->height
-            << " data points from chair_3a.pcd"
-            << std::endl;
+  std::cerr << "PointCloud before cutting: " << cloud->width * cloud->height 
+       << " data points (" << pcl::getFieldsList (*cloud) << ").";
 
-cloud_a = *input;
+//cutting module
+int diff = cloud->width * cloud->height - 2048;
+output = *cloud;
 
+if(diff > 0){
+std::cout << "Need to delete " << diff << " points" << std::endl;
+output.width = 2048;
+output.height = 1;
+output.points.resize(output.width * output.height);
+}
+else if(diff < 0){
+std::cout << "Need to add " << -diff << " points" << std::endl;
+middle = output;
+middle.width = -diff;
+middle.height = 1;
+middle.points.resize(middle.width * middle.height);
+for(int i = 0; i < middle.width * middle.height; i++){
+middle.points[i].x = middle.points[i].x + middle.points[i].x/((std::rand()%100)+100);
+middle.points[i].y = middle.points[i].y + middle.points[i].y/((std::rand()%100)+100);
+middle.points[i].z = middle.points[i].z + middle.points[i].z/((std::rand()%100)+100);
+std::cout << middle.points[i] << " " << output.points[i] << std::endl;
+};
+output += middle;
+};
 
-if (pcl::io::loadPCDFile<pcl::PointXYZ> ("chair_3b.pcd", *input) == -1) //* load the file
-  {
-    PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
-    return (-1);
-  }
-std::cout << "Loaded "
-            << input->width * input->height
-            << " data points from chair_3a.pcd"
-            << std::endl;
+std::cerr << "PointCloud after filtering: " << output.width * output.height << " data points " << std::endl;
 
-cloud_b = *input;
-
-output = cloud_a + cloud_b;
-
-std::cout << "Concatenation has "
-            << output.width * output.height
-            << " data points and will be saved as wynik_3.pcd"
-	    << std::endl;
-
-pcl::io::savePCDFileASCII ("wynik.pcd", output);
+pcl::io::savePCDFileASCII (cloud_name + "_2048.pcd", output);
 
   return (0);
 }
-
